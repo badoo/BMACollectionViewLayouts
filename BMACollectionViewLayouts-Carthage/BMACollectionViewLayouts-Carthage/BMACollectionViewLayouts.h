c2367ed06de759751a72ff58e80804d93c3a21f1
alexander.balaban <alexander.balaban@corp.badoo.com>
2018-02-01 16:05:35 +0000

    rename framework

---
+++ BMACollectionViewLayouts-Carthage/BMACollectionViewLayouts-Carthage/BMACollectionViewLayouts.h
@@ -0,0 +1,19 @@
+//
+//  BMACollectionViewLayouts.h
+//  BMACollectionViewLayouts
+//
+//  Created by alexander.balaban on 01/02/2018.
+//  Copyright © 2018 badoo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BMACollectionViewLayouts.
+FOUNDATION_EXPORT double BMACollectionViewLayoutsVersionNumber;
+
+//! Project version string for BMACollectionViewLayouts.
+FOUNDATION_EXPORT const unsigned char BMACollectionViewLayoutsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BMACollectionViewLayouts/PublicHeader.h>
+#import <BMACollectionViewLayouts/BMAReorderableFlowLayout.h>
+
